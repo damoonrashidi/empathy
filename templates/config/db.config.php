5c4166a748f3479697c3b42d2e93298fc75aaa65
Damoon Rashidi <damoon.rashidi@freespee.com>
2017-02-09 18:56:17 +0100

    updated dbconfig template to look at port as well

--- templates/config/db.config.php
+++ templates/config/db.config.php
@@ -1,13 +1,13 @@
 <?php
-  
-  $conf = Spyc::YAMLLoad(".empathy.yaml");
-  $env = "development";
-  
-  $GLOBALS["MYSQL"] = mysqli_connect(
-    $conf['db'][$env]["host"],
-    $conf['db'][$env]["username"],
-    $conf['db'][$env]["password"],
-    $conf['db'][$env]["database"]
-  ) or die ("Error establishing a connection to the database");
+
+$conf = Spyc::YAMLLoad(".empathy.yaml");
+$env = "development";
+
+$GLOBALS["MYSQL"] = mysqli_connect(
+  $conf['db'][$env]["host"] . ":" . $conf['db'][$env]["port"],
+  $conf['db'][$env]["username"],
+  $conf['db'][$env]["password"],
+  $conf['db'][$env]["database"]
+) or die("Error establishing a connection to the database");
 
 ?>
